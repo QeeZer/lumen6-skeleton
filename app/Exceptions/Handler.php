dd15ab5ac868cb063a636b97c0a8e7835aedcd1a
qeezer <184353381@qq.com>
2021-12-28 10:27:15 +0800

    fix: compose script fix && handler exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use QeeZer\ApiResponder\Contracts\BusinessException;
 use QeeZer\ApiResponder\Helpers;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -27,7 +28,7 @@ class Handler extends ExceptionHandler
         ModelNotFoundException::class,
         ValidationException::class,
 
-        ApiException::class,
+        BusinessException::class,
     ];
 
     /**
