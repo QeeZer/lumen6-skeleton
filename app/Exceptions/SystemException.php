41dc91acdbce3b666f56fa5723921500dfcc1212
qeezer <184353381@qq.com>
2022-03-03 14:23:37 +0800

    fix: systemException && phpstan && git commit hooks.

--- app/Exceptions/SystemException.php
+++ app/Exceptions/SystemException.php
@@ -9,4 +9,8 @@ use QeeZer\ApiResponder\Contracts\NondisclosureException;
 
 class SystemException extends Exception implements NondisclosureException
 {
+    public function __construct(Exception $exception)
+    {
+        parent::__construct($exception->getMessage(), $exception->getCode(), $exception->getPrevious());
+    }
 }
